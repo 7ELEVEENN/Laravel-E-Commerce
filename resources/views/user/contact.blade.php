5387598fd394461039f791fa232b03d890badf2b
7ELEVEENN <venndelossantos@gmail.com>
2023-10-24 14:56:51 +0800

    Contact Us page

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -130,7 +130,7 @@ https://templatemo.com/tm-546-sixteen-clothing
       4. Copy only URL and paste it within the src="" field below
   -->
               <div id="map">
-                <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="330px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.8285573514995!2d123.37361631934932!3d13.405970313207247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a199a3e62f19ed%3A0xd70d166421a51a16!2sCamarines%20Sur%20Polytechnic%20Colleges!5e1!3m2!1sen!2sph!4v1698130291512!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </div>
             <div class="col-md-4">
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -44,7 +44,11 @@ https://templatemo.com/tm-546-sixteen-clothing
     <!-- Header -->
     <header class="">
       <nav class="navbar navbar-expand-lg">
+        
         <div class="container">
+          
+          <!--<img src="{{ asset('assets/images/icon.png') }}" alt="Logo" height="40"> -->
+
           <a class="navbar-brand" href="{{ url('/') }}"><h2>Book <em>Strap</em></h2></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
